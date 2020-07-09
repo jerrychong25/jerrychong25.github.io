84bbca1bd0caca010e240a3ee9b19456d4bdf2e6
Jerry Chong <jerrychong25@gmail.com>
2020-07-09 23:14:58 +0800

    Synced dev latest changes to staging

---
+++ src/components/home/index.js
@@ -0,0 +1,26 @@
+import { h, Component } from 'preact';
+import fetch from 'unfetch';
+
+import Header from './header';
+import Profile from './profile';
+import Experience from './experience';
+import Skills from './skills';
+import Footer from './footer';
+
+export default class Home extends Component {
+	state = {
+		profile: require('../../profile.json')
+	};
+
+	render( {}, { profile } ) {
+		return (
+			<div itemscope itemtype="https://schema.org/Person">
+				{profile.header && <Header {...profile.header} />}
+				{profile.profile && <Profile {...profile.profile} />}
+				{profile.experience && <Experience {...profile.experience} />}
+				{profile.skills && <Skills {...profile.skills} />}
+				{profile.contact && <Footer {...profile.contact} />}
+			</div>
+		);
+	}
+}
